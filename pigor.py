776e327faf461d37f9e951c04cd43b47baea4db3
Nico Einsidler <nicoeinsidler@Claudias-iMac.local>
2018-12-15 15:53:52 +0100

    added help menu

--- pigor.py
+++ pigor.py
@@ -3,3 +3,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+PROGRAM_NAME = "PIGOR"
+
+def print_header(text):
+    """
+    This function prints a beautiful header followed by one empty line.
+    """
+    print("\n" + "=" * len(text))
+    print(text)
+    print("=" * len(text) + "\n")
+
+    # print quick help
+    print_help(display="quick")
+
+def print_help(display="all"):
+    """
+    Prints a help menu on the screen for the user.
+
+    display    ... specify the lenght of the help menu, options are 'all' or 'quick'
+
+    """
+    cmd_dict = {
+        'h' :   "displays a help menu",
+        'q' :   "quit {}".format(PROGRAM_NAME)
+    }
+
+    if display == "quick":
+        print("{:.<4} {}".format("h",cmd_dict['h']))
+        print("{:.<4} {}".format("q",cmd_dict['q']))
+    else:
+        for k,v in cmd_dict.items():
+            print("{:.<4} {}".format(k,v))
+    print("\n")
+
+print_header("Welcome to {}.".format(PROGRAM_NAME))
+while True:
+    cmd = input()
+    if cmd == "q":
+        break
+    elif cmd == "h":
+        print_help()
+    else:
+        pass
