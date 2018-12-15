098475d306a5d68b65b220057ff80b805db83695
Nico Einsidler <nicoeinsidler@Claudias-iMac.local>
2018-12-15 18:21:10 +0100

    added read_data method

--- classes/measurement.py
+++ classes/measurement.py
@@ -18,12 +18,39 @@ class Measurement:
             :param directory: the directory of the file to read data from
             :param file_name: the name of the file to read from (containing
                               also its file extention)
+
+        Returns nothing.
         """
         self.directory = directory
         self.file_name = file_name
 
-        self.read_data(self, directory, file_name)
+        try:
+            self.read_data(directory, file_name)
+        except IOError as e20:
+            print(e20)
+        except Exception as e:
+            print(e)
+        
+        
+
+    def read_data(self, directory, file_name):
+        """
+        Reads data from file and stores it in the object.
+
+            :param self: the object itself
+            :param directory: the directory of the file to read data from
+            :param file_name: the name of the file to read from (containing
+                              also its file extention)
+        
+        Returns nothing if successfull, but rasies exception if not.
+        """
+
+        self.raw = [line.rstrip('\n') for line in open(directory + file_name)]
+        
+
+
+
 
 
 if __name__ == "__main__":
-    pass
+    msrmt = Measurement("./testfiles/","2018-12-11-1000-dc1com.dat")
