f050ba4cb42a19c253b3f2ae424f9ed33586aef4
Nico Einsidler <nicoeinsidler@Claudias-iMac.local>
2018-12-16 18:45:28 +0100

    added find_all_files()

--- pigor.py
+++ pigor.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
 
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 from os import walk
 import measurement
 
+# name of the program
 PROGRAM_NAME = "PIGOR"
 
-f = []
-d = []
-for (dirpath, dirnames, filenames) in walk("."):
-    f.extend(filenames)
-    d.extend(dirnames)
-    break
-
-print(f)
-print(d)
 
 def print_header(text):
     """
@@ -48,14 +41,41 @@ def print_help(display="all"):
             print("{:.<4} {}".format(k,v))
     print("\n")
 
+def find_all_files(directory="."):
+    f = []
+    d = []
+    
+    for (dirpath, dirnames, filenames) in walk(directory):
+        f.extend(filenames)
+        d.extend(dirnames)
+        break
+    
+    # filter all dat files
+    f = [a for a in f if re.search(r".dat$", a)]
+    #print(
+    #    "Found {} dat files in {} to analyze: \n{}".format(len(f),directory,f)
+    #)
+
+    # filter directories
+    d = [_ for _ in d if not (_.startswith('.') or _.startswith('__'))]
+
+    # check if there are subdirs
+    if len(d) > 0:
+        for _ in d:
+            f.extend(find_all_files(_))
+
+    return f
+
 
 # starting main loop
 print_header("Welcome to {}.".format(PROGRAM_NAME))
 while True:
+    
     cmd = input()
     if cmd == "q":
         break
     elif cmd == "h":
         print_help()
     else:
-        pass
+        f = find_all_files()
+        print(f)
