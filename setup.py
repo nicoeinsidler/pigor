3c485f9fd8248f404cb1ba7c9304c5c9ebf369ca
Nico Einsidler <nicoeinsidler@gmail.com>
2019-03-20 23:37:41 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='measurement',
+      version='1.0',
+      description='Measurement Module for NEPTUN, TU Wien',
+      author='Nico Einsidler',
+      author_email='nicoeinsidler@gmail.com',
+      url='www.neutroninterferometry.com/',
+      py_modules=['measurement'],
+      )
