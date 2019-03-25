7a697477f7e4c050f24579e26f71c2b50b6d6bd9
Nico Einsidler <nicoeinsidler@gmail.com>
2019-03-25 21:18:01 +0100

    decorator to register functions in PIGOR

--- pigor.py
+++ pigor.py
@@ -11,6 +11,20 @@ from pathlib import Path
 # name of the program
 PROGRAM_NAME = "PIGOR"
 
+# functions that the user can utilize
+USER_FUNCTIONS = dict()
+
+
+def show_user(func):
+    """Register a function to be displayed to the user as an option"""
+
+    try:
+        cmd = re.search('\[(.+?)\]', func.__doc__).group(1)
+    except AttributeError as e:
+        print(e)
+
+    USER_FUNCTIONS[func.__name__] = (cmd, func)
+    return func
 
 def print_header(text):
     """This function prints a beautiful header followed by one empty line.
@@ -23,26 +37,29 @@ def print_header(text):
     print("=" * len(text) + "\n")
 
     # print quick help
-    print_help(display="quick")
+    print_help(display="all")
 
+@show_user
 def print_help(display="all"):
-    """Prints a help menu on the screen for the user.
+    """Prints a help menu on the screen for the user. This function can be used by the command [h].
 
     :param display: specify the lenght of the help menu, options are 'all' or 'quick' (Default value = "all")
 
     """
-    cmd_dict = {
-        'h' :   "displays a help menu",
-        'q' :   "quit {}".format(PROGRAM_NAME)
-    }
-
-    if display == "quick":
-        print("{:.<4} {}".format("h",cmd_dict['h']))
-        print("{:.<4} {}".format("q",cmd_dict['q']))
+    # print a list of all available commands
+    if display == 'all':
+        for k,v in USER_FUNCTIONS.items():
+            print(f'{v[0]} ... {k}')
+        print(f'q ... quit {PROGRAM_NAME}\n')
+    # show only the function that should be displayed, but this time with a docstring
     else:
-        for k,v in cmd_dict.items():
-            print("{:.<4} {}".format(k,v))
-    print("\n")
+        try:
+            f = USER_FUNCTIONS[display]
+            print(f'{display}: \n\n {f[1].__doc__}\n\n')
+        except Exception as e:
+            print(e)
+            raise ValueError
+
 
 def find_all_files():
     """Finds all dat files recursively in all subdirectories ignoring hidden directories
@@ -54,9 +71,9 @@ def find_all_files():
     """
     return [Path(path) for path in glob.glob('**/*.dat', recursive=True)]
 
-
-def analyse_files(filepaths):
-    """Analyses all given files in list.
+@show_user
+def analyse_files(filepaths='all'):
+    """Analyses all given files in list. This function can be used by the command [a].
 
     :param filepaths:   list of files to analyse with
                         their relative dir path added
@@ -70,7 +87,9 @@ def analyse_files(filepaths):
     #     m = measurement.Measurement(p)
     #     m.plot()
     #     m.export_meta(html=True)
-        
+    
+    if filepaths == 'all':
+        filepaths = find_all_files()
 
     with mp.Pool() as pool:
         m = pool.map(measurement.Measurement, filepaths)
@@ -85,21 +104,19 @@ def main():
     """Main Loop"""
     # starting main loop
     print_header("Welcome to {}.".format(PROGRAM_NAME))
+
     while True:
-        
-        print("To proceed, please press any key.")
+        print("Please type a command you want to perform and press <ENTER>.")
         cmd = input()
         if cmd == "q":
             break
-        elif cmd == "h":
-            print_help()
+        elif cmd in [i[0] for i in list(USER_FUNCTIONS.values())]:
+            for v in USER_FUNCTIONS.values():
+                if v[0] == cmd:
+                    f = v[1]
+            f()
         else:
-            all_files = find_all_files()
-            print(
-                "Found {} dat files to analyze. \nProceeding with analysis...".format(len(all_files))
-            )
-
-            analyse_files(all_files)
+            print('The command you typed does not exist. Press h + <ENTER> for help.')
         
 if __name__ == '__main__':
     mp.freeze_support()
