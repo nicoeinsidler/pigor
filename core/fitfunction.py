a2060796ea9beacc2510045c21b78cc835a58eb4
Nico Einsidler <nicoeinsidler@gmail.com>
2019-04-02 16:26:21 +0200

    added fitfunction class

---
+++ core/fitfunction.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+class FitFunction:
+    """This is a template class for a fit function."""
+
+    def __init__(self):
+        """Creates a new FitFunction object"""
